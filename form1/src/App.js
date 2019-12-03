8126acb5e97f8bdfa697e80558eb9472c2ffea37
yarraballikumarreddy <yarraballikumarreddy116@gmail.com>
2019-12-03 19:44:38 +0530

    first commit

---
+++ form1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Reactformval from './Reactformval/Reactformval'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Reactformval></Reactformval>
+    </div>
+  );
+}
+
+export default App;
